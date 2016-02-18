fca0d74e9b77700fdd65348fd55df641f4d51f41
Ross Blair <rblair2@stanford.edu>
2016-02-17 16:26:35 -0800

    placeholder urls and views for concept, disorder, task search/list

--- cognitive/apps/atlas/api_views.py
+++ cognitive/apps/atlas/api_views.py
@@ -1,12 +1,27 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .query import Task
+from .query import Concept, Disorder, Task
 
+Concept = Concept()
+Disorder = Disorder()
 Task = Task()
 
-class TaskAPIList(APIView):
+class SearchAPI(APIView):
+    def get(self, request, format=None):
+        return Response()
 
+class ConceptAPI(APIView):
+    def get(self, request, format=None):
+        tasks = Concept.all()
+        return Response(tasks)
+
+class TaskAPI(APIView):
     def get(self, request, format=None):
         tasks = Task.all()
+        return Response(tasks)
+
+class DisorderAPI(APIView):
+    def get(self, request, format=None):
+        tasks = Disorder.all()
         return Response(tasks)
--- cognitive/apps/atlas/urls.py
+++ cognitive/apps/atlas/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url,patterns
 
-from . import views
-from .api_views import TaskAPIList
+from . import api_views, views
 
 
 urlpatterns = [
     url(r'^concepts$', views.all_concepts, name="all_concepts"),
     url(r'^concepts/(?P<letter>[a-z]|[A-Z]{1})/$', views.concepts_by_letter, 
         name="concepts_by_letter"),
-    url(r'^api/$', TaskAPIList.as_view(), name='task_api_list'),
 ]
 
+api_urls = [
+    url(r'^api/search$', api_views.SearchAPI.as_view(), name='task_api_list'),
+    url(r'^api/concept$',api_views.ConceptAPI.as_view(), name='task_api_list'),
+    url(r'^api/task$', api_views.TaskAPI.as_view(), name='task_api_list'),
+    url(r'^api/disorder', api_views.DisorderAPI.as_view(), name='task_api_list'),
+]
+
+urlpatterns += api_urls
