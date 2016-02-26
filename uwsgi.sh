161f622a9c54a36a20ae43261b6926e271879d56
Ross Blair <rblair2@stanford.edu>
2016-02-26 15:39:39 -0800

    replaced /app with /code in uwsgi files

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while ! nc -z graphdb 7474; do sleep 3; done
 
-python /app/manage.py makemigrations
-python /app/manage.py migrate
-python /app/manage.py collectstatic --noinput
-uwsgi --ini /app/uwsgi.ini
+python /code/manage.py makemigrations
+python /code/manage.py migrate
+python /code/manage.py collectstatic --noinput
+uwsgi --ini /code/uwsgi.ini
