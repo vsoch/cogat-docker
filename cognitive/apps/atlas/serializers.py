ea7f9896454d6f4f1f5f376bb0294d290684e101
Ross Blair <rblair2@stanford.edu>
2016-02-26 09:38:14 -0800

    mereged urls.py for atlas and moved new files from vsoch/master up a directory level

--- cognitive/apps/atlas/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-from .query import Task
-
-class TaskSerializer(serializers.Serializer):
-    id = serializers.CharField()
-    name = serializers.CharField()
